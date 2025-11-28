b0fae0d495ce4902efc72861c26a8d2a4cea0b6c
Laspard <tt.smartman@gmail.com>
2025-11-28 15:00:05 +0530

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -13,4 +13,10 @@
 
 ]]
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66bc6d30278bc08919bec8cde5e15fea.lua"))()
+--[[loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66bc6d30278bc08919bec8cde5e15fea.lua"))()]]
+
+local players = game:GetService("Players")
+local plr = players.LocalPlayer
+
+setclipboard(tostring("https://discord.gg/5MWnDsEnbw"))
+plr:Kick("outdated script. get latest script at dsc.gg/officialhorizonhub or it is copied in ur clipboard!")
